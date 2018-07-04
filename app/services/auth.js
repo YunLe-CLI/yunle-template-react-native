632e29908e1c8080902984376003591053d34fe8
hexiao-o <460559761@qq.com>
2018-07-04 22:51:28 +0800

    delete

--- app/services/auth.js
+++
@@ -1,6 +0,0 @@
-import { delay } from '../utils'
-
-export const login = async () => {
-  await delay(2000)
-  return true
-}
--- app/utils/index.js
+++
@@ -1,7 +0,0 @@
-export { NavigationActions, StackActions } from 'react-navigation'
-
-export { default as Storage } from './storage'
-
-export const delay = time => new Promise(resolve => setTimeout(resolve, time))
-
-export const createAction = type => payload => ({ type, payload })
