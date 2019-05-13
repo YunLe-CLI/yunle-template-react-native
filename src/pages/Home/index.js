b891c1283a6ab07bfef799683990280eaa65f806
hexiao-o <460559761@qq.com>
2019-05-13 12:37:24 +0800

    add antd ui

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text } from 'react-native';
 import { withApollo } from 'react-apollo';
 import { connect } from 'react-redux';
 import { StackActions, NavigationActions } from 'react-navigation'; // Version can be specified in package.json
-
+import { Button } from '@ant-design/react-native';
 @connect(({ app }) => {
     return app;
 })
@@ -12,17 +12,16 @@ class HomeScreen extends React.Component {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>Home Screen</Text>
-                <Button
-                    title="Go to Details"
-                    onPress={() => {
-                        this.props.navigation.dispatch(StackActions.reset({
-                            index: 0,
-                            actions: [
-                                NavigationActions.navigate({ routeName: 'Details' })
-                            ],
-                        }))
-                    }}
-                />
+                <Button onPress={() => {
+                    this.props.navigation.dispatch(StackActions.reset({
+                        index: 0,
+                        actions: [
+                            NavigationActions.navigate({ routeName: 'Details' })
+                        ],
+                    }))
+                }}>
+                    Go to Details
+                </Button>
             </View>
         );
     }
