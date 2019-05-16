4ec194e5fd087a400bb9c0293e3dca5db5452587
hexiao-o <460559761@qq.com>
2019-05-16 13:19:23 +0800

    add .jshintrc

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     ['@babel/plugin-proposal-decorators', { 'legacy': true }],
-    ["@babel/plugin-proposal-class-properties", { "loose": true }],
     ["import", { libraryName: "@ant-design/react-native" }]
   ]
 };
