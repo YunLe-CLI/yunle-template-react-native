3944b60379d3f1f9a850d0188ad7249649299875
大狗吱 <dagouzhi@qq.com>
2020-01-21 00:27:05 +0800

    fix

--- src/theme/Theme_Education/pages/index.ts
+++ src/theme/Theme_Education/pages/index.ts
@@ -3,7 +3,6 @@ export { default as Splash } from './Splash';
 
 export { default as Home } from './Home';
 export { default as Me } from './Me';
-export { default as VideoPlayer } from './VideoPlayer';
 // 资料
 export { default as Assets } from './Assets';
 // 视频详情
