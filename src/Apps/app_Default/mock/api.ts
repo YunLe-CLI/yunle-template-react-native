72fb6fb568ab63bcdee82a85f5d453579281f947
大狗吱 <dagouzhi@qq.com>
2020-03-18 23:24:07 +0800

    fix

--- src/Apps/app_Default/mock/api.ts
+++ src/Apps/app_Default/mock/api.ts
@@ -4,8 +4,8 @@ export default {
   '/users': {
     'users|1-5': [
       { 
-        'id': ()=>Random.integer(20,100),
-        'name': ()=>Random.ctitle(),
+        'id': ()=> Random.integer(20,100),
+        'name': ()=> Random.ctitle(),
       }
     ]
   }
